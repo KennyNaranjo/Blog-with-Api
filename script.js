be9e6c05ece74ce2e2e7daf19f2869f966e3d772
Dayan Alvarez <vbdam91@gmail.com>
2022-12-13 17:58:42 +0100

    updating styles and html

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-urlPosts = "http://localhost:3000/posts"
-urlUsers = "http://localhost:3000/users"
-urlComments = "http://localhost:3000/comments"
-parentElement = document.querySelector(".parent-box");
-modalBody = document.querySelector(".modal-body");
-modalHeader = document.querySelector(".modal-header");
-
-    // const modalTittle = document.createElement ("h5")
-    // modalTittle.textContent = titlePost;
-    // modalHeader.append(modalTittle);
-
+const urlPosts = "http://localhost:3000/posts"
+const urlUsers = "http://localhost:3000/users"
+const urlComments = "http://localhost:3000/comments"
+const parentElement = document.querySelector(".parent-box");
+const postTitle = document.querySelector(".post-title");
+const postText = document.querySelector(".post-text");
 
 
 fetch(urlPosts)
 .then(responsePost => responsePost.json())
 .then(dataPost => {
     dataPost.forEach( post => {
-        const articlePost = document.createElement("article");
-        const titlePost = document.createElement("h5");
-        articlePost.setAttribute("data-bs-toggle","modal")
-        articlePost.setAttribute("data-bs-target", "#modal-id")
-        articlePost.classList = "col bg-primary m-3 p-2 rounded numPost "
-        titlePost.textContent = post.title;
-        titlePost.classList = "p-2";    
-        articlePost.append(titlePost);
-        parentElement.appendChild(articlePost);
-        
-        
-        
+        allData(post)
     });
-    
 });
 
 
+function allData(post){
+    const articlePost = document.createElement("article");
+    const titlePost = document.createElement("h5");
+    const imgCard = document.createElement("img")
+    articlePost.setAttribute("data-bs-toggle","modal");
+    articlePost.setAttribute("data-bs-target", "#modal-id");
+    articlePost.classList = "col bg-primary m-2 p-2 rounded text-center";
+    
+    titlePost.textContent = post.title;
+    postTitle.textContent = post.title;
+    postText.textContent = post.body;
+    imgCard.classList = "img-thumbnail rounded"
+    imgCard.setAttribute("src", "4kfigures.jpg")
+    
+    titlePost.classList = "p-2";
+    articlePost.append(imgCard, titlePost);
+    
+    parentElement.appendChild(articlePost);
+    
+   
+}
 
 // fetch(urlUsers)
 // .then(responseUsers => responseUsers.json())

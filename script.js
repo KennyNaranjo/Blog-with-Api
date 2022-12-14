3a29fda5f1e3568e2585ab2abbc9dff6162f7064
Dayan <vbdam91@gmail.com>
2022-12-14 17:31:29 +0100

    testing comments button

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const postTitle = document.querySelector(".post-title");
 const postText = document.querySelector(".post-text");
 const modalId = document.querySelector("#modal-id");
 const modalBody = document.querySelector(".modal-body");
-const modalUserName = document.querySelector(".user-name")
-const modalEmail = document.querySelector(".email")
+const modalUserName = document.querySelector(".user-name");
+const modalEmail = document.querySelector(".email");
+const modalComments = document.querySelector(".btn-info");
 
 fetch(urlPosts)
 .then(responsePost => responsePost.json())
@@ -19,7 +20,7 @@ fetch(urlPosts)
 });
 
 
-function allData(post){
+function allData(post) {
     const articlePost = document.createElement("article");
     const titleCard = document.createElement("h6");
     const imgCard = document.createElement("img");
@@ -30,26 +31,27 @@ function allData(post){
     imgCard.setAttribute("data-id", post.id);
     titleCard.setAttribute("data-id", post.id);
     articlePost.setAttribute("data-id", post.id);
-    articlePost.classList = "col bg-secondary m-2 p-2 rounded text-center";
+    articlePost.classList = "col bg-secondary m-2 p-2 rounded text-center article-selector";
     titleCard.classList = "p-2 text-white";
     imgCard.classList = "img-fluid";
     titleCard.textContent = post.title;
     articlePost.addEventListener("click", triggerModal);
+    modalComments.addEventListener("click", triggerModalComments)
     articlePost.append(imgCard, titleCard);
     parentElement.appendChild(articlePost);
     
 }
 
 function triggerModal(e) {
-    const postID = e.target.getAttribute("data-id");
-    fetch("http://localhost:3000/posts/" + postID)
+    const dataId = e.target.getAttribute("data-id");
+    fetch("http://localhost:3000/posts/" + dataId)
     .then(responsePostId => responsePostId.json())
     .then(data => {
         postTitle.textContent = data.title;
         postText.textContent = data.body;
         fetch("http://localhost:3000/users/" + data.userId)
         .then(responseUserId => responseUserId.json())
-        .then( data => {
+        .then(data => {
             modalUserName.textContent = data.username;
             modalEmail.textContent = data.email;
         });
@@ -57,12 +59,28 @@ function triggerModal(e) {
     })
 }
 
+fetch(urlComments)
+.then(responsePost => responsePost.json())
+.then(dataPost => {
+    dataPost.forEach( data => {
+        allDataComments(data);
+    });
+
+});
 
 
-// fetch(urlUsers)
-// .then(responseUsers => responseUsers.json())
-// .then(dataUsers => );
+function allDataComments(data){
+    console.log(data.postId)
+    // articles.setAttribute("comment-id", data.postId)
+}
 
-// fetch(urlComments)
-// .then(responseComments => responseComments.json())
-// .then(dataComments => )
+
+function triggerModalComments(e) {
+    const article = e.target.getAttribute("comment-id");
+    fetch("http://localhost:3000/comments/" )
+    .then(responsePostId => responsePostId.json())
+    .then(data => console.log(data.postId))
+
+}
+
+

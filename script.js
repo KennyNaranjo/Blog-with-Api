35243f83a3078f8d5f8ef9c0be976d143e5acd72
Dayan Alvarez <vbdam91@gmail.com>
2022-12-14 08:56:29 +0100

    fixing styles

--- script.js
+++ script.js
@@ -4,35 +4,32 @@ const urlComments = "http://localhost:3000/comments"
 const parentElement = document.querySelector(".parent-box");
 const postTitle = document.querySelector(".post-title");
 const postText = document.querySelector(".post-text");
-
+const modalId = document.querySelector("#modal-id")
 
 fetch(urlPosts)
 .then(responsePost => responsePost.json())
 .then(dataPost => {
     dataPost.forEach( post => {
-        allData(post)
+        allData(post);
     });
 });
 
 
 function allData(post){
     const articlePost = document.createElement("article");
-    const titlePost = document.createElement("h5");
-    const imgCard = document.createElement("img")
+    const titleCard = document.createElement("h6");
+    const imgCard = document.createElement("img");
+    imgCard.setAttribute("src", "./assets/img/4kfigures.jpg");
     articlePost.setAttribute("data-bs-toggle","modal");
-    articlePost.setAttribute("data-bs-target", "#modal-id" + post.id);
-    articlePost.setAttribute("role", "button")
-    articlePost.classList = "col bg-secondary m-2 p-2 rounded text-center ";
-    
-    titlePost.textContent = post.title;
+    articlePost.setAttribute("data-bs-target", "#modal-id");
+    articlePost.setAttribute("role", "button");
+    articlePost.classList = "col bg-secondary m-2 p-2 rounded text-center";
+    titleCard.classList = "p-2 text-white";
+    imgCard.classList = "img-fluid";
+    titleCard.textContent = post.title;
     postTitle.textContent = post.title;
-    postText.textContent = post.body;
-    imgCard.classList = "img-thumbnail"
-    imgCard.setAttribute("src", "4kfigures.jpg")
-    
-    titlePost.classList = "p-2 text-white";
-    articlePost.append(imgCard, titlePost);
-    
+    postText.textContent = post.body
+    articlePost.append(imgCard, titleCard);
     parentElement.appendChild(articlePost);
     
    

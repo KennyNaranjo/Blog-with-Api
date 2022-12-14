5aa054eb17ac349ddd2fe85d64b4e2fda6a57bb7
Dayan Alvarez <vbdam91@gmail.com>
2022-12-14 12:43:44 +0100

    adding inside modal: title, body

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const urlPosts = "http://localhost:3000/posts"
-const urlUsers = "http://localhost:3000/users"
-const urlComments = "http://localhost:3000/comments"
+const urlPosts = "http://localhost:3000/posts";
+const urlUsers = "http://localhost:3000/users";
+const urlComments = "http://localhost:3000/comments";
 const parentElement = document.querySelector(".parent-box");
 const postTitle = document.querySelector(".post-title");
 const postText = document.querySelector(".post-text");
-const modalId = document.querySelector("#modal-id")
+const modalId = document.querySelector("#modal-id");
+const modalBody = document.querySelector(".modal-body");
+const modalUserName = document.querySelector(".user-name")
+const modalEmail = document.querySelector(".email")
 
 fetch(urlPosts)
 .then(responsePost => responsePost.json())
@@ -12,6 +15,7 @@ fetch(urlPosts)
     dataPost.forEach( post => {
         allData(post);
     });
+
 });
 
 
@@ -23,17 +27,37 @@ function allData(post){
     articlePost.setAttribute("data-bs-toggle","modal");
     articlePost.setAttribute("data-bs-target", "#modal-id");
     articlePost.setAttribute("role", "button");
+    imgCard.setAttribute("data-id", post.id);
+    titleCard.setAttribute("data-id", post.id);
+    articlePost.setAttribute("data-id", post.id);
     articlePost.classList = "col bg-secondary m-2 p-2 rounded text-center";
     titleCard.classList = "p-2 text-white";
     imgCard.classList = "img-fluid";
     titleCard.textContent = post.title;
-    postTitle.textContent = post.title;
-    postText.textContent = post.body
+    articlePost.addEventListener("click", triggerModal);
     articlePost.append(imgCard, titleCard);
     parentElement.appendChild(articlePost);
     
-   
 }
+
+function triggerModal(e) {
+    const postID = e.target.getAttribute("data-id");
+    fetch("http://localhost:3000/posts/" + postID)
+    .then(responsePostId => responsePostId.json())
+    .then(data => {
+        postTitle.textContent = data.title;
+        postText.textContent = data.body;
+        fetch("http://localhost:3000/users/" + data.userId)
+        .then(responseUserId => responseUserId.json())
+        .then( data => {
+            modalUserName.textContent = data.username;
+            modalEmail.textContent = data.email;
+        });
+        
+    })
+}
+
+
 
 // fetch(urlUsers)
 // .then(responseUsers => responseUsers.json())

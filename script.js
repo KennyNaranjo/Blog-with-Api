56474e6d336a1c30211e337790df291e94d76029
Dayan <vbdam91@gmail.com>
2022-12-15 13:29:24 +0100

    fixing buttons delete-edit

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const modalEmail = document.querySelector(".email");
 const modalComments = document.querySelector(".btn-info");
 const modalFooter = document.querySelector(".modal-footer");
 const comments = document.querySelector("#comments");
+const buttonBox = document.querySelector(".button-box");
 
 
 fetch(urlPosts)
@@ -27,9 +28,7 @@ function allData(post) {
     const articlePost = document.createElement("article");
     const titleCard = document.createElement("h6");
     const imgCard = document.createElement("img");
-    const bttnEditPost = document.createElement("button");
-    const bttnDeletePost = document.createElement ("button");
-    const boxBttn = document.createElement("div");
+   
 
 
     imgCard.setAttribute("src", "./assets/img/1.gif");
@@ -38,19 +37,16 @@ function allData(post) {
     articlePost.setAttribute("role", "button");
     imgCard.setAttribute("data-id", post.id);
     titleCard.setAttribute("data-id", post.id);
-    
+    articlePost.setAttribute("data-id", post.id);
     articlePost.classList = "col bg-dark m-2 p-2 rounded text-center article-selector ";
     titleCard.classList = "p-2  text-white  text-center ";
     imgCard.classList = "img-fluid rounded rounded-bottom " ;
     titleCard.textContent = post.title;
     articlePost.addEventListener("click", triggerModal);
     articlePost.append(imgCard, titleCard);
-    boxBttn.append(bttnEditPost, bttnDeletePost);
-    bttnEditPost.textContent = "Edit"
-    bttnDeletePost.textContent = "Delete"
-    boxBttn.classList = "d-flex justify-content-center"
+
     parentElement.appendChild(articlePost);
-    articlePost.appendChild(boxBttn);
+
     
 }
 

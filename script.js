4d0e40137e2024fda82f7e99c121d126dfa4ae12
Dayan <vbdam91@gmail.com>
2022-12-16 04:25:49 +0100

    Merge branch 'java-script' into dev

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// QuerySelectors 
+
 const parentElement = document.querySelector(".parent-box");
 const postTitle = document.querySelector(".post-title");
 const postText = document.querySelector(".post-text");
@@ -11,17 +13,19 @@ const comments = document.querySelector("#comments");
 const deletePost = document.querySelector(".delete");
 const modalClass = document.querySelector(".modal")
 
+
+// API call, creating elements dynamically, giving them values in each post-card, and adding some classes and attributes
+
 fetch("http://localhost:3000/posts")
 .then(responsePost => responsePost.json())
 .then(dataPost => {
     dataPost.forEach( post => {
-        allData(post);
+        postData(post);
     });
 });
 
-
-function allData(post) {
-    const articlePost = document.createElement("article");
+function postData(post) {
+    const articlePost = document.createElement("article");  
     const titleCard = document.createElement("h6");
     const imgCard = document.createElement("img");
     imgCard.setAttribute("src", "./assets/img/1.gif");
@@ -40,8 +44,10 @@ function allData(post) {
     parentElement.appendChild(articlePost);
 }
 
+
+//API calls, adding classes, adding values when the modal appears
+
 function triggerModal(element) {
-    comments.textContent= "";
     const dataId = element.target.getAttribute("data-id");
     fetch("http://localhost:3000/posts/" + dataId)
     .then(responsePostId => responsePostId.json())
@@ -57,10 +63,15 @@ function triggerModal(element) {
         });
     })
 
+
+    //API call, creating elements dynamically, adding classes and adding values when the comments appears
+
     fetch("http://localhost:3000/comments?postId=" + dataId)
         .then(responsePost => responsePost.json())
         .then(data => {
+            comments.textContent="";
             data.forEach( i => {
+                
                 const commentBox = document.createElement("div");
                 const paragraph = document.createElement("p");
                 const titlePost = document.createElement("h6");
@@ -72,16 +83,18 @@ function triggerModal(element) {
                 commentEmail.textContent = i.email;
                 emailBox.append(commentIcon, commentEmail);
                 commentBox.append(titlePost, paragraph, emailBox);
-                commentIcon.classList = "fa-regular fa-envelope d-inline m-2"
-                commentBox.classList = "card p-2 p m-3 bg-dark text-white"
-                emailBox.classList = "mt-2 mb-3"
-                titlePost.classList = "m-2"
-                paragraph.classList = "p-2"
-                commentEmail.classList = "d-inline m-2 border p-1 rounded bg-secondary text-white"
+                commentIcon.classList = "fa-regular fa-envelope d-inline m-2";
+                commentBox.classList = "card p-2 p m-3 bg-dark text-white";
+                emailBox.classList = "mt-2 mb-3";
+                titlePost.classList = "m-2";
+                paragraph.classList = "p-2";
+                commentEmail.classList = "d-inline m-2 border p-1 rounded bg-secondary text-white";
                 comments.append(commentBox);
             })
-            deletePost.addEventListener("click", postDelete)
-            
+
+            // API call and deleting object in db.json
+
+            deletePost.addEventListener("click", postDelete);
             function postDelete(){
                 
                 Swal.fire({
